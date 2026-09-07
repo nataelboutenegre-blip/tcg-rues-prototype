3e7eb285cddcee474ea58327dafe50a67263f342
Nataël <nataelboutenegre@gmail.com>
2026-09-07 14:56:26 +0200

    Add TIER_RANK and DEPT_NAMES for enhanced data

--- app.js
+++ app.js
@@ -61,13 +61,22 @@ function tierFor(pop){
 }
 
 let TIER_BUCKETS = {};
+let TIER_RANK = {};
+
+const DEPT_NAMES = {"01":"Ain","02":"Aisne","03":"Allier","04":"Alpes-de-Haute-Provence","05":"Hautes-Alpes","06":"Alpes-Maritimes","07":"Ardèche","08":"Ardennes","09":"Ariège","10":"Aube","11":"Aude","12":"Aveyron","13":"Bouches-du-Rhône","14":"Calvados","15":"Cantal","16":"Charente","17":"Charente-Maritime","18":"Cher","19":"Corrèze","21":"Côte-d'Or","22":"Côtes-d'Armor","23":"Creuse","24":"Dordogne","25":"Doubs","26":"Drôme","27":"Eure","28":"Eure-et-Loir","29":"Finistère","2A":"Corse-du-Sud","2B":"Haute-Corse","30":"Gard","31":"Haute-Garonne","32":"Gers","33":"Gironde","34":"Hérault","35":"Ille-et-Vilaine","36":"Indre","37":"Indre-et-Loire","38":"Isère","39":"Jura","40":"Landes","41":"Loir-et-Cher","42":"Loire","43":"Haute-Loire","44":"Loire-Atlantique","45":"Loiret","46":"Lot","47":"Lot-et-Garonne","48":"Lozère","49":"Maine-et-Loire","50":"Manche","51":"Marne","52":"Haute-Marne","53":"Mayenne","54":"Meurthe-et-Moselle","55":"Meuse","56":"Morbihan","57":"Moselle","58":"Nièvre","59":"Nord","60":"Oise","61":"Orne","62":"Pas-de-Calais","63":"Puy-de-Dôme","64":"Pyrénées-Atlantiques","65":"Hautes-Pyrénées","66":"Pyrénées-Orientales","67":"Bas-Rhin","68":"Haut-Rhin","69":"Rhône","70":"Haute-Saône","71":"Saône-et-Loire","72":"Sarthe","73":"Savoie","74":"Haute-Savoie","75":"Paris","76":"Seine-Maritime","77":"Seine-et-Marne","78":"Yvelines","79":"Deux-Sèvres","80":"Somme","81":"Tarn","82":"Tarn-et-Garonne","83":"Var","84":"Vaucluse","85":"Vendée","86":"Vienne","87":"Haute-Vienne","88":"Vosges","89":"Yonne","90":"Territoire de Belfort","91":"Essonne","92":"Hauts-de-Seine","93":"Seine-Saint-Denis","94":"Val-de-Marne","95":"Val-d'Oise"};
 
 function buildIndex(){
   TIER_BUCKETS = {};
+  TIER_RANK = {};
   for(const t of TIERS) TIER_BUCKETS[t.id] = [];
   COMMUNES.forEach((c, i) => {
     TIER_BUCKETS[tierFor(c[2]).id].push(i);
   });
+  for(const t of TIERS){
+    const bucket = TIER_BUCKETS[t.id];
+    bucket.sort((a, b) => COMMUNES[b][2] - COMMUNES[a][2]); // population decroissante
+    bucket.forEach((idx, i) => { TIER_RANK[idx] = i + 1; });
+  }
 }
 
 function pickTier(){
@@ -85,7 +94,7 @@ function drawOne(){
   const bucket = TIER_BUCKETS[tier.id];
   const idx = bucket[Math.floor(Math.random() * bucket.length)];
   const c = COMMUNES[idx];
-  return { nom: c[0], dept: c[1], pop: c[2], nbVoies: c[3], lat: c[4], lon: c[5], code: c[6], tier };
+  return { nom: c[0], dept: c[1], pop: c[2], nbVoies: c[3], lat: c[4], lon: c[5], code: c[6], tier, rank: TIER_RANK[idx], tierSize: bucket.length };
 }
 
 let session = {commun:0, peucommun:0, rare:0, legendaire:0, total:0};
@@ -211,8 +220,9 @@ function makeCardEl(draw){
           <div class="rarity-tag" style="background:${draw.tier.color}">${draw.tier.label}</div>
           <p class="commune-name">${draw.nom}</p>
           <div class="fields">
-            <div class="field"><span>Département</span><b>${draw.dept}</b></div>
+            <div class="field"><span>Département</span><b>${DEPT_NAMES[draw.dept] || draw.dept}</b></div>
             <div class="field"><span>Population</span><b>${draw.pop.toLocaleString('fr-FR')}</b></div>
+            <div class="field"><span>Numéro</span><b>${draw.rank.toLocaleString('fr-FR')} / ${draw.tierSize.toLocaleString('fr-FR')}</b></div>
           </div>
         </div>
       </div>
